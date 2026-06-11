86709d4e5bfd7b0fbf4a8ef703386b87364fa356
winter-sil/nce <soulxz2007@gmail.com>
2026-06-10 20:36:25 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,20 +13,10 @@ local delfile = delfile or function(file)
 	writefile(file, '')
 end
 
-local downloader = Instance.new('TextLabel')
-downloader.Size = UDim2.new(1, 0, 0, 40)
-downloader.BackgroundTransparency = 1
-downloader.TextStrokeTransparency = 0
-downloader.TextSize = 20
-downloader.TextColor3 = Color3.new(1, 1, 1)
-downloader.Font = Enum.Font.Arial
-downloader.Text = ''
-downloader.Parent = Instance.new('ScreenGui', gethui and gethui() or cloneref(game:GetService('CoreGui')))
-
 local function downloadFile(path, func)
 	if not isfile(path) then
 		if not license.Closet then
-			downloader.Text = 'bananavxpe is downloading: "'.. path .. '"'
+			print("[BananaVape] Downloading: " .. path)
 		end
 		local suc, res = pcall(function()
 			return game:HttpGet('https://raw.githubusercontent.com/SolentraXminishakk/BananaVape/'..readfile('bananavxpe/profiles/commit.txt')..'/'..select(1, path:gsub('bananavxpe/', '')), true)
@@ -38,7 +28,6 @@ local function downloadFile(path, func)
 			res = '--This watermark is used to delete the file if its cached, remove it to make the file persist after vape updates.\n'..res
 		end
 		writefile(path, res)
-		downloader.Text = ''
 	end
 	return (func or readfile)(path)
 end
@@ -56,10 +45,9 @@ local function wipeFolder(path)
 	end
 end
 
-
 for _, folder in {'bananavxpe', 'bananavxpe/games', 'bananavxpe/profiles', 'bananavxpe/assets', 'bananavxpe/libraries', 'bananavxpe/guis'} do
 	if not isfolder(folder) then
-		downloader.Text = 'downloading: '.. folder
+		print("[BananaVape] Downloaded Folder: " .. folder)
 		makefolder(folder)
 	end
 end
@@ -86,5 +74,4 @@ if not shared.VapeDeveloper then
 	writefile('bananavxpe/profiles/commit.txt', commit)
 end
 
-downloader.Text = ''
 return loadstring(downloadFile('bananavxpe/main.lua'), 'main')(license)
